23eb3fe5fbca2c9a1ff309c4d3f6df346ef8bf79
Dhaval <dudhat.dhaval.m@gmail.com>
2019-08-27 21:52:53 +0530

    Updates

---
+++ precache-manifest.6471e236f1683a1297b0d6d5dde4e7f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f24577a2fec007dfd6670b17d093d2a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f830f2a5e6f68f78e15b",
+    "url": "/monsters-rolodex/static/css/main.03016a88.chunk.css"
+  },
+  {
+    "revision": "4b7594f9ba93f757104a",
+    "url": "/monsters-rolodex/static/js/2.765d0f25.chunk.js"
+  },
+  {
+    "revision": "f830f2a5e6f68f78e15b",
+    "url": "/monsters-rolodex/static/js/main.67b41b12.chunk.js"
+  },
+  {
+    "revision": "f7fff3448ef51b7cb626",
+    "url": "/monsters-rolodex/static/js/runtime~main.edd7f36b.js"
+  }
+]);
